7cca1b257116cb91755710138a5ecee1af894a2a
wilsonfrancisco <wilson.gs.francisco@gmail.com>
2024-07-14 16:04:05 +0100

    feat: add upload feature to the app using uploadthing

--- components/file-upload.tsx
+++ components/file-upload.tsx
@@ -1,5 +1,7 @@
 "use cliente"
 
+import { X } from "lucide-react"
+import Image from "next/image"
 import { UploadDropzone } from "@/lib/uploadthing"
 
 interface FileUploadProps {
@@ -13,6 +15,23 @@ export const FileUpload: React.FC<FileUploadProps> = ({
   onChange,
   value
 }) => {
+  const fileType = value.split(".").pop()
+
+  if (value && fileType !== "pdf") {
+    return (
+      <div className="relative h-20 w-20">
+        <Image fill src={value} alt="uploaded server image" className="rounded-full" />
+        <button
+          className="bg-rose-500 text-white p-1 rounded-full absolute top-0 right-0 shadow-sm"
+          type="button"
+          onClick={() => onChange("")}
+        >
+          <X className="h-4 w-4" />
+        </button>
+      </div>
+    )
+  }
+
   return (
     <UploadDropzone
       endpoint={endpoint}
