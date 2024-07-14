ab5641cefe72d6188371744f997b2c9aa3957625
wilsonfrancisco <wilson.gs.francisco@gmail.com>
2024-07-14 03:23:54 +0100

    clerck setup

--- app/(auth)/(routes)/register/page.tsx
+++
@@ -1,5 +0,0 @@
-const RegisterPage = () => {
-  return (<p>Register page</p>);
-}
-
-export default RegisterPage;
--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Open_Sans } from "next/font/google";
 
 import "./styles/globals.css";
+import { ClerkProvider } from "@clerk/nextjs";
 
 const font = Open_Sans({ subsets: ["latin"] });
 
@@ -21,8 +22,10 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <body className={font.className}>{children}</body>
-    </html>
+    <ClerkProvider>
+      <html lang="en">
+        <body className={font.className}>{children}</body>
+      </html>
+    </ClerkProvider>
   );
 }
