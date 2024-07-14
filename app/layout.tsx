87b84ea6ec3cbc21931ddd5e0a6fff16cec4d901
wilsonfrancisco <wilson.gs.francisco@gmail.com>
2024-07-14 13:26:03 +0100

    Initial modal setup

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,7 +2,7 @@ import type { Metadata } from "next";
 import { Open_Sans } from "next/font/google";
 import { ClerkProvider } from "@clerk/nextjs";
 
-import { ThemeProvider } from "@/components/theme-provider";
+import { ThemeProvider } from "@/components/providers/theme-provider";
 
 import "./styles/globals.css";
 import { cn } from "@/lib/utils";
@@ -26,7 +26,6 @@ export default function RootLayout({
 }>) {
   return (
     <ClerkProvider>
-
       <html lang="en" suppressHydrationWarning>
         <body className={cn(
           font.className,
---
+++ lib/initial-profile.ts
@@ -0,0 +1,32 @@
+import { currentUser, auth } from "@clerk/nextjs/server";
+
+import prisma from "@/lib/prisma/client";
+
+export const initialProfile = async () => {
+  const user = await currentUser()
+
+  if (!user) {
+    return auth().redirectToSignIn()
+  }
+
+  const profile = await prisma.profile.findUnique({
+    where: {
+      userId: user.id
+    }
+  })
+
+  if (profile) {
+    return profile
+  }
+
+  const newProfile = await prisma.profile.create({
+    data: {
+      userId: user.id,
+      name: `${user.firstName} ${user.lastName}`,
+      imageUrl: user.imageUrl,
+      email: user.emailAddresses[0].emailAddress
+    }
+  })
+
+  return newProfile
+}
