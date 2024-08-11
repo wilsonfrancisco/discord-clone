a54623645276c9da230f14fd71d5560376c6d63c
wilsonfrancisco <wilson.gs.francisco@gmail.com>
2024-08-11 15:47:56 +0100

    feat: websocket configuration

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,6 +4,7 @@ import { ClerkProvider } from "@clerk/nextjs";
 
 import { ThemeProvider } from "@/components/providers/theme-provider";
 import { ModalProvider } from "@/components/providers/modal-provider";
+import { SocketProvider } from "@/components/providers/socket-provider";
 
 import "./styles/globals.css";
 import { cn } from "@/lib/utils";
@@ -38,8 +39,10 @@ export default function RootLayout({
             storageKey="discord-theme"
             disableTransitionOnChange
           >
-            <ModalProvider />
-            {children}
+            <SocketProvider>
+              <ModalProvider />
+              {children}
+            </SocketProvider>
           </ThemeProvider>
         </body>
       </html>
